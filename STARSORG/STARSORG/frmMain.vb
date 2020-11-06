6df777f329633d803b5a727630c1054209859b51
Sultan Abuhaqab <sabuh008@fiu.edu>
2020-11-06 17:49:03 +0300

    Implemented mouse event handler and form load event handler

--- STARSORG/STARSORG/frmMain.vb
+++ STARSORG/STARSORG/frmMain.vb
@@ -1,3 +1,55 @@
 ﻿Public Class frmMain
+    Private Sub tsbProxy_MouseEnter(sender As Object, e As EventArgs) Handles tsbCourse.MouseEnter, tsbEvents.MouseEnter,
+        tsbHelp.MouseEnter, tsbHome.MouseEnter, tsbLogout.MouseEnter, tsbMember.MouseEnter, tsbRole.MouseEnter, tsbRSVP.MouseEnter,
+        tsbSemester.MouseEnter, tsbTutor.MouseEnter
+        'We need to do this only because we are not putting our images in the Image property of the toolbar buttons
 
+        Dim tsbProxy As ToolStripButton
+        tsbProxy = DirectCast(sender, ToolStripButton)
+        tsbProxy.DisplayStyle = ToolStripItemDisplayStyle.Text
+
+    End Sub
+
+    Private Sub tsbProxy_MouseLeave(sender As Object, e As EventArgs) Handles tsbCourse.MouseLeave, tsbEvents.MouseLeave,
+        tsbHelp.MouseLeave, tsbHome.MouseLeave, tsbLogout.MouseLeave, tsbMember.MouseLeave, tsbRole.MouseLeave, tsbRSVP.MouseLeave,
+        tsbSemester.MouseLeave, tsbTutor.MouseLeave
+        'We need to do this only because we are not putting our images in the Image property of the toolbar buttons
+
+        Dim tsbProxy As ToolStripButton
+        tsbProxy = DirectCast(sender, ToolStripButton)
+        tsbProxy.DisplayStyle = ToolStripItemDisplayStyle.Image
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Try
+            myDB.OpenDB()
+        Catch ex As Exception
+            MessageBox.Show("Unable to open database. Connection string = " & gstrConn, "DB Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            EndProgram()
+        End Try
+    End Sub
+
+    Private Sub EndProgram()
+        'Close each form except main
+        Dim f As Form
+        Me.Cursor = Cursors.WaitCursor
+
+        For Each f In Application.OpenForms
+            If f.Name <> Me.Name Then
+                If Not f Is Nothing Then
+                    f.Close()
+                End If
+            End If
+        Next
+
+        'Close the database connection
+        If Not objSQLConn Is Nothing Then
+            objSQLConn.Close()
+            objSQLConn.Dispose()
+        End If
+
+        Me.Cursor = Cursors.Default
+    End Sub
 End Class
--- STARSORG/STARSORG/modDBvb.vb
+++ STARSORG/STARSORG/modDBvb.vb
@@ -4,6 +4,6 @@ Module modDBvb
 
     Public objSQLConn As SqlConnection
     Public objSQLCommand As SqlCommand
-    Public gstrConn As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=D:\Development\VisualStudio\WindowsProgramming\sample-8-SultanAbuhaqab\HenryBooks\HBooks_2019.mdf;Integrated Security=True"
+    Public gstrConn As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=D:\Development\VisualStudio\WindowsProgramming\project-SultanAbuhaqab\STARSORG\STARSDB.mdf;Integrated Security=True"
 
 End Module
