2631f64cf52f607ee2d86d393d2eb03be8c9028f
Sultan Abuhaqab <sabuh008@fiu.edu>
2020-11-06 16:49:43 +0300

    Initialised project, imported required modules and attached database connection

---
+++ STARSORG/STARSORG/modDBvb.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Data.SqlClient
+
+Module modDBvb
+
+    Public objSQLConn As SqlConnection
+    Public objSQLCommand As SqlCommand
+    Public gstrConn As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=D:\Development\VisualStudio\WindowsProgramming\sample-8-SultanAbuhaqab\HenryBooks\HBooks_2019.mdf;Integrated Security=True"
+
+End Module
