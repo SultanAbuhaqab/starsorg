1afb993e12d37fe3da51b468cb24455c1a4e5248
Ulises Marcelino <umarc001@fiu.edu>
2020-12-02 19:44:04 -0500

    WIPE ALL

--- STARSORG/STARSORG/modDB.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports System.Data.SqlClient
-Module modDB
-    Public objSQLConn As SqlConnection
-    Public objSqlCommand As SqlCommand
-    Public objSQLDR As SqlDataReader
-    Public gstrConn As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=D:\Development\VisualStudio\WindowsProgramming\starsorg\STARSORG\STARSDB.mdf;Integrated Security=True"
-End Module
