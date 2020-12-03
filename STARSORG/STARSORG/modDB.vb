39fa6a856ef96cbf9e9cd0afbe4cfffb43804f3f
Sultan Abuhaqab <sabuh008@fiu.edu>
2020-12-03 10:07:36 +0300

    Initial merge change to update mod db url

--- STARSORG/STARSORG/modDB.vb
+++ STARSORG/STARSORG/modDB.vb
@@ -3,5 +3,5 @@ Module modDB
     Public objSQLConn As SqlConnection
     Public objSqlCommand As SqlCommand
     Public objSQLDR As SqlDataReader
-    Public gstrConn As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=C:\Users\Ulises\Desktop\Team1\starsorg\STARSORG\STARSDB.mdf;Integrated Security=True"
+    Public gstrConn As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=D:\Development\VisualStudio\WindowsProgramming\starsorg\STARSORG\STARSDB.mdf;Integrated Security=True"
 End Module
